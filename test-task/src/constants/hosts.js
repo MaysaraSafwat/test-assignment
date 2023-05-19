10932c93a054cdf8e65f3378d5a78d980ae2e3de
Maysara Safwat <maisarasafwat2@gmail.com>
2023-05-19 13:36:57 +0300

    //

--- test-task/src/constants/hosts.js
+++ test-task/src/constants/hosts.js
@@ -1,5 +1,5 @@
 //const hostURL = 'https://juniortesttask1.000webhostapp.com/products'
-const hostURL ='http://localhost/scandiweb-test-task/server/products'
-//const hostURL = 'https://juniortesttask2.000webhostapp.com/products'
+//const hostURL ='http://localhost/scandiweb-test-task/server/products'
+const hostURL = 'https://juniortesttask2.000webhostapp.com/products'
 
 export default hostURL;
